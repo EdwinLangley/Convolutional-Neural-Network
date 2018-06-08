8536bad62aba3375a918346280cf36ae0a7479a5
Edwin Langley <Edlangley@live.co.uk>
2018-06-08 18:31:33 +0100

    Removed useless comments

--- start.py
+++ start.py
@@ -1,38 +1,6 @@
-# import sys
-# from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QStyleFactory
-# from PyQt5.QtCore import pyqtSlot
-# from tabs import Ui_MainWindow
-#
-# # class AppWindow(QDialog):
-# #     def __init__(self):
-# #         super().__init__()
-# #         self.ui = Ui_MainWindow()
-# #         self.ui.setupUi(self)
-# #         print("hi")
-# #         self.show()
-# #
-# #     @pyqtSlot()
-# #     def on_pushButton_clicked(self):
-# #         print("hi")
-#
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     app.setStyle(QStyleFactory.create('Fusion'))
-#     ex = Ui_MainWindow()
-#     w = QMainWindow()
-#     ex.setupUi(w)
-#     w.show()
-#     sys.exit(app.exec_())
-#
-#     app.run_cat.clicked.connect(hello)
-#
-# def hello():
-#     print("Hello")
-
 import sys
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QStyleFactory
-from tabs import Ui_MainWindow
 
 class Mainwindow():
 
