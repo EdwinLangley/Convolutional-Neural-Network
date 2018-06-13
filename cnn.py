1783feb195dbe8a92d084de3fa42af34b98eec9c
Edwin Langley <Edlangley@live.co.uk>
2018-06-13 17:21:02 +0100

    Categorical not category

--- cnn.py
+++ cnn.py
@@ -42,16 +42,16 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
-                                                 class_mode = 'binary')
+                                                 class_mode = 'categorical')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
-                                            class_mode = 'binary')
+                                            class_mode = 'categorical')
 
 classifier.fit_generator(
                          training_set,
-                         steps_per_epoch = 8000,
+                         steps_per_epoch = 80,
                          epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 10)
