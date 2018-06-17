26735b149f340a269c8b3ad99ef19250ed2d6205
Edwin Langley <Edlangley@live.co.uk>
2018-06-17 15:22:54 +0100

    Results appear within the GUI

--- cnn.py
+++ cnn.py
@@ -9,110 +9,121 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras.preprocessing import image
 import os
+from keras.preprocessing.image import ImageDataGenerator
 
 
-def load_image(img_path, show=False):
+class NNet:
 
-    img = image.load_img(img_path, target_size=(64, 64))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
-
-    if show:
-        plt.imshow(img_tensor[0])                           
-        plt.axis('off')
-        plt.show()
-
-    return img_tensor
+    def __init__(self):
+        self.classifier = Sequential()
 
 
-if not os.path.isfile('model.h5'):
-  # Initialising the CNN
-  classifier = Sequential()
+    def conv_pool_layers(self):
+        self.classifier.add(Convolution2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
-  # Step 1 - Convolution
-  classifier.add(Convolution2D(32, ( 3, 3) , input_shape = (64, 64, 3), activation = 'relu'))
+        # Step 2 - Pooling
+        self.classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-  # Step 2 - Pooling
-  classifier.add(MaxPooling2D(pool_size = (2, 2)))
+        # Adding a second convolutional layer
+        self.classifier.add(Convolution2D(64, (3, 3), activation="relu"))
+        self.classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-  # Adding a second convolutional layer
-  classifier.add(Convolution2D(64, (3, 3), activation="relu"))
-  classifier.add(MaxPooling2D(pool_size = (2, 2)))
+        self.classifier.add(Convolution2D(128, (3, 3), activation="relu"))
+        self.classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-  classifier.add(Convolution2D(128, (3, 3), activation="relu"))
-  classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-  classifier.add(Convolution2D(256, (3, 3), activation="relu"))
-  classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-  # Step 3 - Flattening
-  classifier.add(Flatten())
-
-  # Step 4 - Full connection
-  classifier.add(Dense(activation="relu", units=128))
-  classifier.add(Dense(activation="softmax", units=10))
-
-  # Compiling the CNN
-  classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-
-  # Part 2 - Fitting the CNN to the images
-
-  from keras.preprocessing.image import ImageDataGenerator
-
-  train_datagen = ImageDataGenerator(
-                                    rescale = 1./255,
-                                    shear_range = 0.2,
-                                    zoom_range = 0.2,
-                                    horizontal_flip = True)
-
-  test_datagen = ImageDataGenerator(
-                                    rescale = 1./255)
-
-  training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                   target_size = (64, 64),
-                                                   batch_size = 16,
-                                                   class_mode = 'categorical')
-
-  test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                              target_size = (64, 64),
-                                              batch_size = 16,
-                                              class_mode = 'categorical')
-
-  classifier.fit_generator(
-                           training_set,
-                           steps_per_epoch = 80,
-                           epochs = 25,
-                           validation_data = test_set,
-                           workers=100,
-                           max_queue_size = 100,
-                           validation_steps = 10,
-                           verbose=1,
-                           use_multiprocessing=False)
-
-  classifier.save('model.h5')
-
-else:
-  classifier = load_model('model.h5')
- 
-
-img_path = 'dataset/new/4.jpg'
-
-new_image = load_image(img_path)
-
-# check prediction
-pred = classifier.predict(new_image)
-
-print(pred)
+        self.classifier.add(Convolution2D(256, (3, 3), activation="relu"))
+        self.classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 
-labels = ['Danaus plexippus', 'Heliconius charitonius', 'Heliconius erato', 'Junonia coenia', 'Lycaena phlaeas', 'Nymphalis antiopa', 'Papilio cresphontes', 'Pieris rapae',
-         'Vanessa atalanta', 'Vanessa cardui']
+    def flattening(self):
+        self.classifier.add(Flatten())
 
-colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'cyan', 'crimson', 'pink', 'sienna', 'olive', 'lime']
 
-patches, texts = plt.pie(pred[0], colors=colors, shadow=True, startangle=90)
-plt.legend(patches, labels, loc="best")
-plt.axis('equal')
-plt.tight_layout()
-plt.show()
+    def full_connection(self):
+        # Step 4 - Full connection
+        self.classifier.add(Dense(activation="relu", units=128))
+        self.classifier.add(Dense(activation="softmax", units=10))
+
+        # Compiling the CNN
+        self.classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+    def gen_train_test(self):
+        self.train_datagen = ImageDataGenerator(
+            rescale=1. / 255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+
+        self.test_datagen = ImageDataGenerator(
+            rescale=1. / 255)
+
+        self.training_set = self.train_datagen.flow_from_directory('dataset/training_set',
+                                                         target_size=(64, 64),
+                                                         batch_size=16,
+                                                         class_mode='categorical')
+
+        self.test_set = self.test_datagen.flow_from_directory('dataset/test_set',
+                                                    target_size=(64, 64),
+                                                    batch_size=16,
+                                                    class_mode='categorical')
+
+
+    def save_model(self, path):
+        self.classifier.save(path)
+
+    def load_model(self, path):
+        self.classifier = load_model(path)
+
+
+    def fit_data_to_model(self):
+        self.classifier.fit_generator(
+            self.training_set,
+            steps_per_epoch=80,
+            epochs=25,
+            validation_data=self.test_set,
+            workers=100,
+            max_queue_size=100,
+            validation_steps=10,
+            verbose=1,
+            use_multiprocessing=False)
+
+
+    def make_prediction_on_model(self, path):
+
+        self.new_image = self.load_image(path)
+
+        # check prediction
+        self.pred = self.classifier.predict(self.new_image)
+
+        print(self.pred)
+
+
+    def make_pie_chart(self):
+        self.labels = ['Danaus plexippus', 'Heliconius charitonius', 'Heliconius erato', 'Junonia coenia', 'Lycaena phlaeas',
+                  'Nymphalis antiopa', 'Papilio cresphontes', 'Pieris rapae',
+                  'Vanessa atalanta', 'Vanessa cardui']
+
+        self.colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'cyan', 'crimson', 'pink', 'sienna', 'olive',
+                  'lime']
+
+        self.patches, texts = plt.pie(self.pred[0], colors=self.colors, shadow=True, startangle=90)
+        plt.legend(self.patches, self.labels, loc="best")
+        plt.axis('equal')
+        plt.tight_layout()
+        plt.savefig('IMG/last_pie.png',bbox_inches='tight')
+
+    def load_image(self, img_path, show=False):
+        img = image.load_img(img_path, target_size=(64, 64))
+        img_tensor = image.img_to_array(img)  # (height, width, channels)
+        img_tensor = np.expand_dims(img_tensor,
+                                        axis=0)  # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+        img_tensor /= 255.  # imshow expects values in the range [0, 1]
+
+        if show:
+            plt.imshow(img_tensor[0])
+            plt.axis('off')
+            plt.show()
+
+        return img_tensor
+
--- start.py
+++ start.py
@@ -3,6 +3,11 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QStyleFactory, QFileDialog
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QSize
+from cnn import NNet
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 class Mainwindow():
 
@@ -10,14 +15,23 @@ class Mainwindow():
         self.dlg = uic.loadUi("GUI.ui")
         self.dlg.run_cat.clicked.connect(self.run_category)
         self.dlg.open_target_btn.clicked.connect(self.open_image)
-        self.dlg.save_output_btn.clicked.connect(self.save_output)
         self.dlg.run_train_btn.clicked.connect(self.run_train)
         self.dlg.imgaug_info_btn.clicked.connect(self.img_aug_dialog)
         self.dlg.passes_info_btn.clicked.connect(self.passes_dialog)
+        self.dlg.load_model_btn.clicked.connect(self.load_model_handle)
+        self.dlg.save_model_btn.clicked.connect(self.save_model_handle)
         self.dlg.show()
 
     def run_category(self):
-        print("run_category: TODO")
+
+        nn.make_prediction_on_model(self.comparison_image)
+        nn.make_pie_chart()
+
+        self.graph_image =  QImage("IMG/last_pie.png")
+        self.graph_scaled_image = self.graph_image.scaled(QSize(630, 470))
+        self.dlg.results_image.setPixmap(QPixmap.fromImage(self.graph_scaled_image))
+        self.dlg.results_image.show()
+
 
     def open_image(self):
         """
@@ -26,24 +40,24 @@ class Mainwindow():
         """
         self.comparison_image, _ = QFileDialog.getOpenFileName(None, "Open Image", "~","Image Files (*.jpg *.png)")
         self.comparisonicon = QImage(self.comparison_image)
-        self.comparisonscaledimage = self.comparisonicon.scaled(QSize(250, 250))
+        self.comparisonscaledimage = self.comparisonicon.scaled(QSize(380, 380))
         self.dlg.selected_image.setPixmap(QPixmap.fromImage(self.comparisonscaledimage))
         self.dlg.target_img_src.setText(self.comparison_image)
         self.dlg.selected_image.show()
 
-    def save_output(self):
-        """
-
-        :return:
-        """
-        print("save_output: TODO")
 
     def run_train(self):
         """
 
         :return:
         """
-        print("run_train: TODO")
+
+        nn.conv_pool_layers()
+        nn.flattening()
+        nn.full_connection()
+        nn.gen_train_test()
+        nn.fit_data_to_model()
+
 
     def img_aug_dialog(self):
         """
@@ -58,6 +72,14 @@ class Mainwindow():
         :return:
         """
         self.passes_info_dlg = InfoDialogPasses()
+
+
+    def load_model_handle(self):
+        nn.load_model("model.h5")
+
+
+    def save_model_handle(self):
+        nn.save_model("model.h5")
 
 
 class InfoDialogAug():
@@ -76,7 +98,9 @@ class InfoDialogPasses():
         self.dlg.show()
 
 
+
 if __name__ == "__main__":
+    nn = NNet()
     app = QApplication([])
     app.setStyle(QStyleFactory.create('Fusion'))
     form = Mainwindow()
