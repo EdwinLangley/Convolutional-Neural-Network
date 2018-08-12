da6565b4d9c06aeaa1b8347d563dbc344eb1a37f
Edwin Langley <Edlangley@live.co.uk>
2018-08-12 14:09:31 +0100

    Added a results page

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
+import sqlite3
 
 from cnn import NNet
+
+path_to_db = 'db/predictions.db'
 
 nnet = NNet()
 
@@ -31,6 +34,11 @@ def about():
 def contribute():
     return render_template('contribute.html')
 
+@app.route('/results')
+def results():
+    Results = db.get_all_entries()
+    return render_template('results.html', Results=Results)
+
 class PredictionDetails(Form):
     photo = FileField('Photo', validators=[FileAllowed(photos, u'Image only!'), FileRequired(u'File was empty!')])
     name = TextField('Name:', validators=[validators.required()])
@@ -41,19 +49,56 @@ class PredictionDetails(Form):
 def predict():
     form = PredictionDetails(request.form)
 
-    if request.method == 'POST': #and 'photo' in request.files:
+    if request.method == 'POST' and 'photo' in request.files:
         name = request.form['name']
         email = request.form['email']
         filename = photos.save(request.files['photo'])
-        print(name, email)
+        print(name, email, filename)
+        db.add_prediction_to_db(name,email,filename)
         
         #nnet.run_train()
         # return filename
     return render_template('predict.html', form=form)
 
 
+class database_object():
 
+    def create_connection(self):
+        self.conn = sqlite3.connect(path_to_db)
+
+    def create_table(self):
+        self.create_connection()
+        c = self.conn.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS predictions(
+            time TEXT,
+            name TEXT,
+            email TEXT,
+            fileLocation TEXT
+        );""")
+        self.conn.commit()
+        self.conn.close()
+
+    def add_prediction_to_db(self, name, email,filename):
+        self.create_connection()
+        c = self.conn.cursor()
+        c.execute("INSERT INTO predictions(time,name, email,fileLocation) VALUES(CURRENT_TIMESTAMP, '" + name + "','" + email + "','" + filename + "');")
+        self.conn.commit()
+        self.conn.close()
+
+    def get_all_entries(self):
+        self.create_connection()
+        c = self.conn.cursor()
+        cur = c.execute("SELECT * FROM predictions")
+        Results = [dict(Time=row[0],
+                    Name=row[1],
+                    Email=row[2],
+                    FileName=row[3]) for row in cur.fetchall()]
+        self.conn.close()
+        return Results
 
 
 if __name__ == '__main__':
+    db = database_object()
+    db.create_table()
     app.run(debug=True)
+    
